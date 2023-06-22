0b882f5ccb3105f131ecece72eddd3e16b5f8b52
MalcarAle <alexandre.malcar@gmail.com>
2023-06-22 19:11:23 -0300

    add datbaase with sqlite

--- src/server.js
+++ src/server.js
@@ -1,14 +1,16 @@
 require("express-async-errors")
 
-const express = require("express")
+const database = require("./database/sqlite")
 const AppError = require("./utils/AppError")
+const express = require("express")
 const routes = require("./routes")
 
 //APP = API
 const app = express()
 app.use(express.json())
-
 app.use(routes)
+
+database()
 
 app.use((error, request, response, next) => {
   if (error instanceof AppError) {
